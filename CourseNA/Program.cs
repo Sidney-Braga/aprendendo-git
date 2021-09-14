69ca58ce9a669de37fd01d7d09d6d107b052f4a6
Sidney Braga <juniorbraga1011@gmail.com>
2021-09-14 20:11:47 -0300

    Mudança teste

--- CourseNA/Program.cs
+++ CourseNA/Program.cs
@@ -7,6 +7,8 @@ namespace CourseNA {
 
             Produto p = new Produto();
 
+            Console.WriteLine("Mudança teste!");
+
             Console.WriteLine("Entre os dados do produto:");
             Console.Write("Nome: ");
             p.Nome = Console.ReadLine();
